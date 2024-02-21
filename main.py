224fd00c7f3c0499ce06e4a2beba3ad4b4f3959d
Tommy Cheng <115842289+TommyCheng023@users.noreply.github.com>
2024-02-21 18:48:37 -0500

    Revert "Matthew Yan Cart"

--- main.py
+++
@@ -1,6 +0,0 @@
-from pageLogic import create_app
-
-app = create_app()
-
-if __name__ == "__main__":
-    app.run(debug=True)
--- pageLogic/models.py
+++
@@ -1,6 +0,0 @@
-from . import db
-
-class CartItem(db.Model):
-    __tablename__ = 'CartItem'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
