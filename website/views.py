8f68645177346549854637ded333a7c339a9091d
tiffany <tiff0207@bu.edu>
2024-02-21 21:10:03 -0500

    加了个print看看

--- website/views.py
+++ website/views.py
@@ -14,6 +14,6 @@ def home():
     if request.method == 'POST':
         something = request.form['something']
         session['something'] = something
-        flash('已加入session', category='success')
+        flash(session['something'] + '已加入session', category='success')
         print(session['something'])
     return render_template("home.html")
