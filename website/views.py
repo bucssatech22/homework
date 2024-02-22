16da3111c6def0046c1c6bbcf2178cf1347a7664
tiffany <tiff0207@bu.edu>
2024-02-22 10:39:48 -0500

    update for cart page

--- website/views.py
+++ website/views.py
@@ -20,9 +20,5 @@ def home():
 
 @views.route('/cart', methods=['GET', 'POST'])
 def cart():
-    if request.method == 'POST':
-        something = request.form['something']
-        session['something'] = something
-        flash(session['something'] + '已加入session', category='success')
-        print(session['something'])
+
     return render_template("cart.html")
