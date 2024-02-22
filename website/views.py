fb2109abd69ca4d2858beb17c45a8280cf554abc
tiffany <tiff0207@bu.edu>
2024-02-21 19:15:08 -0500

    cssa shopping cart

---
+++ website/views.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, render_template
+from flask import Flask, render_template, request, redirect, session, url_for, flash
+views = Blueprint('views', __name__)
+
+
+# add sign in page, log in page, grocery page, add to list page, 
+
+
+
+
+
+@views.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        something = request.form['something']
+        session['something'] = something
+        flash('已加入session', category='success')
+    return render_template("home.html")
